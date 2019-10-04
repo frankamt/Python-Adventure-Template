2f4d85cf4edcdef687f3d1bec5ba90d6b1a06057
frankamt <fmatthesthompson@brooklynfriends.org>
2019-10-04 19:02:35 +0000

    message

--- adventure.py
+++ adventure.py
@@ -40,7 +40,7 @@ def setup():
     global HP
     global MP
     #Our variable "name" is used to store our name, captured by keyboard input.
-    name = input("What is your name, earth hero?")
+    name = input("What is your name, explorer?")
     #randint is a great way of adding some variety to your players statistics through randomness
     HP = randint(5,20)
     MP = randint(5,20)
@@ -50,14 +50,14 @@ def villager():
     global npcname
     global response
     #Below is a list, we can store lots of things in a list and then retrieve them later.
-    responses = ["Hi", "Are you a hero?", "Are you from this village?", "There has been a dark shadow cast across the village"]
+    responses = ["Hi", "Are you an explorer?", "Are you from this base?", "There is a snow storm heading towards research base"]
     npcnamechoice = ["Roger", "Dexter", "Sarah", "Susan"]
     #Shuffle will shuffle the list contents into a random order.
     shuffle(npcnamechoice)
     npcname = npcnamechoice[0]
     print ("\n["+npcname+":] Hello, my name is "+npcname+", Would you like to talk to me?\n")
     shuffle(responses)
-    print ("Press y to talk to the villager")
+    print ("Press y to talk to the pengiun")
     if input() == "y":
         print ("%s: %s" % (npcname, responses[0]))
     else:
@@ -88,7 +88,7 @@ global HP
 global MP
 global move
 global enemyHP
-print ("Welcome to the land of Narule, %s" % name)
+print ("Welcome to the land of Antarctica, %s" % name)
 #Sleep is Python's way of pausing the game for a specified number of seconds
 sleep(2)
 #Below we are using the helper functions to join a string of text to an integer via the str() helper.
@@ -97,27 +97,27 @@ print ("Your magic skill is" + " " + str(MP))
 
 
 
-print ("Would you like to venture out into the land? Press y then enter to continue")
+print ("Would you like to venture out onto the tundra? Press y then enter to continue")
 #Below we use input to ask for user input, and if it is equal to y, then the code underneath is run.
 if input() == "y":
-    print ("You are in your home, with a roaring fireplace in front of you, above the fire you can see your sword and shield")
-    print ("Would you like to take your sword and shield? Press y then enter to continue")
+    print ("You are in your home, with a roaring fireplace in front of you, above the fire you can see your knife and forcefield")
+    print ("Would you like to take your knife and forcefield? Press y then enter to continue")
     if input() == "y":
         #This is a list, and it can store many items, and to do that we "append" items to the list.
         weapons = []
-        weapons.append("sword")
-        weapons.append("shield")
+        weapons.append("knife")
+        weapons.append("forcefield")
         print ("You are now carrying your %s and your %s" % (weapons[0], weapons[1]))
-        print ("Armed with your %s and %s you swing open the door to your home and see a green valley gleaming in the sunshine." % (weapons[0], weapons[1]))
+        print ("Armed with your %s and %s you swing open the door to your home and see endless ice in all directions." % (weapons[0], weapons[1]))
     else:
         print ("You choose not to take your weapons")
-        print ("Armed with your sense of humour, You swing open the door to see a green valley full of opportunity awaiting you.")
+        print ("Armed with your sense of humour, You swing open the door to see endless ice in all direction.")
 else:
-    print ("You stay at home, sat in your favourite chair watching the fire grow colder. The land of Narule no longer has a hero.")
+    print ("You stay at home, sat in your favourite chair watching the fire grow colder. The land of Antarctica no longer has a hero.")
     print ("Game Over")
     sys.exit(0)
 
-print ("In the distance to the north you can see a small village, to the east you can see a river and to the west a field of wild flowers.")
+print ("In the distance to the north you can see a small research base, to the east you can see a storage facilty and to the west a lot of seals.")
 
 #Remember those functions we created at the start of the code? Well here we are using them in the game.
 print ("\n")
@@ -127,14 +127,14 @@ west()
 move = input("Where would you like to go? ")
 if move == 'n':
     print ("\nYou move to the north, walking in the sunshine.")
-    print ("A villager is in your path and greets you")
+    print ("A scientist is in your path and greets you")
 #elif is short for Else If and it means that if the previous condition is false, to check this condition to see if that is true.
 elif move == 'e':
-    print ("\nYou walk to the river which lies to the east of your home.")
-    print ("A villager is in your path and greets you")
+    print ("\nYou walk to the research base which lies to the east of your home.")
+    print ("A scientist is in your path and greets you")
 elif move == 'w':
-    print ("\nYou walk to the field of wild flowers, stopping to take in the beauty")
-    print ("A villager is in your path and greets you\n")
+    print ("\nYou walk to the seals, stopping to take in the beauty")
+    print ("A seal is in your path and greets you\n")
 
 villager()
 enemy()
@@ -146,15 +146,15 @@ if fight == "y":
     while HP > 0:
 #This loop will only work while our characters HP is greater than 0.
         hit = randint(0,5)
-        print ("You swing your sword and cause %s of damage" % str(hit))
+        print ("You swing your knit and cause %s of damage" % str(hit))
         enemyHP = enemyHP - hit
         print (enemyHP)
         enemyhit = randint(0,5)
-        print ("The ogre swings a club at you and causes %s of damage" % str(enemyhit))
+        print ("The pengiun swings a club at you and causes %s of damage" % str(enemyhit))
         HP = HP - enemyhit
         print (HP)
 else:
-    print ("You turn and run away from the ogre")
+    print ("You turn and run away from the pengiun")
 
 print ("This is where this template ends, this is now YOUR world, build your adventure and share it with the world")
 
